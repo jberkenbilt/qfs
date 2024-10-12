0a7c079e1946e4834b324060a215e5610eaac2d1
Jay Berkenbilt <ejb@ql.org>
2024-10-12 09:48:47 -0400

    Version 0.1.2

--- qfs/qfs.go
+++ qfs/qfs.go
@@ -62,7 +62,7 @@ type parser struct {
 // the option's entry is called. Otherwise, the `""` entry is called for
 // positional options.
 
-const Version = "0.1.1"
+const Version = "0.1.2"
 
 type argHandler struct {
 	fn   func(*parser, string) error
